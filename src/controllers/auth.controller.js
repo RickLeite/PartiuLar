aa0a1511f70009aae4851e471f8f4bd09dd91dc8
Patrick Leite <patrick.pimentel5@gmail.com>
2024-10-05 12:42:28 -0300

    Login, Registro, Logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,15 +1,9 @@
 import prisma from '../lib/prisma.js';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 export const register = async (req, res) => {
     const { nome, email, telefone, senha, genero } = req.body;
-
-    console.log('nome:', nome);
-    console.log('email:', email);
-    console.log('telefone:', telefone);
-    console.log('senha:', senha);
-    console.log('genero:', genero);
-
 
     try {
         const hashedPassword = await bcrypt.hash(senha, 10);
@@ -17,31 +11,50 @@ export const register = async (req, res) => {
             data: { nome, email, telefone, senha: hashedPassword, genero },
         });
         res.status(201).json(newUser);
+
+        console.log('newUser:', newUser);
     } catch (error) {
         res.status(500).json({ message: 'Erro ao registrar usuário' });
     }
 };
 
 export const login = async (req, res) => {
-    const { email, password } = req.body;
+    const { email, senha } = req.body;
 
     try {
         const user = await prisma.usuario.findUnique({
             where: { email: email },
         });
 
+        console.log('user:', user);
+
         if (!user) {
             return res.status(400).json({ message: 'Usuário não encontrado' });
         }
 
-        const isMatch = await bcrypt.compare(password, user.senha);
+        const isMatch = await bcrypt.compare(senha, user.senha);
 
         if (!isMatch) {
             return res.status(400).json({ message: 'Senha incorreta' });
         }
 
-        res.status(200).json({ message: 'Login bem-sucedido' });
+        const age = 60 * 60 * 24 * 7; // 1 week de tempo de vida do cookie
+
+        const token = jwt.sign({
+            id: user.id
+        }, "Ye2Xg+0h+k0kiUGoIu62jLTAoYYLTmJ5Zr0idiPSK2Y=", {
+            expiresIn: age,
+        });
+
+        res.cookie("token", token, { httpOnly: true, maxAge: age })
+            .status(200)
+            .json({ message: 'Login bem-sucedido' });
     } catch (error) {
-        res.status(500).json({ message: 'Erro ao fazer login' });
+        console.error(error);
+        res.status(500).json({ message: 'Erro no servidor' });
     }
 };
+
+export const logout = async (req, res) => {
+    res.clearCookie('token').json({ message: 'Logout bem-sucedido' });
+}
